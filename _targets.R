df70a1698037f0100c18a1eb1ee6f30ba7e29684
Beaudan <beaudan.brown@gmail.com>
2025-09-22 16:31:28 +1000

    Merge redcap data and merge pipeline

--- _targets.R
+++ _targets.R
@@ -5,8 +5,9 @@ library(crew)
 
 dotenv::load_dot_env()
 edf_dir <- Sys.getenv("EDF_DIR")
- ncpus <- 3
-#ncpus <- future::availableCores() - 1
+data_dir <- Sys.getenv("DATA_DIR")
+# ncpus <- 3
+ncpus <- future::availableCores() - 1
 
 # Ensure single threaded within targets
 Sys.setenv(R_DATATABLE_NUM_THREADS = 1)
@@ -28,21 +29,6 @@ tar_option_set(
 
 tar_source()
 
-freqs <- data.table(
-  freq = c(11, 15),
-  name = c("slow", "fast")
-)
-
-channels <- data.table(
-  prefix = c("C", "F"),
-  name = c("central", "frontal")
-)
-
-predictors <- data.table(
-  variable = c("COUPL_ANGLE", "COUPL_OVERLAP_Z", "COUPL_MAG_Z"),
-  name = c("angle", "overlap", "mag")
-)
-
 list(
   tar_files(
     edf_files,
@@ -50,6 +36,10 @@ list(
       edfs <- list.files(edf_dir, pattern = "\\.edf$", full.names = TRUE)
       edfs[file.exists(paste0(edfs, ".XML"))]
     }
+  ),
+  tar_target(
+    redcap_data,
+    fread(file.path(data_dir, "data_cleaner.v2.csv"))
   ),
   tar_target(
     edf_results,
@@ -76,13 +66,22 @@ list(
   ),
   tar_target(
     stages,
-    c("N2", "N3")
+    c("N2,N3", "N2", "N3")
+  ),
+  tar_target(
+    outcomes,
+    c("visualrepro2_total", "logicalmem_delay_total")
+  ),
+  tar_target(
+    predictors,
+    c("overlap", "angle", "mag")
   ),
   tar_map(
     values = data.table(
-      stage = c("N2", "N3")
+      stage = c("N2,N3", "N2", "N3"),
+      name = c("N23", "N2", "N3")
     ),
-    names = "stage",
+    names = "name",
     tar_target(
       stage_threshold_results,
       get_stage_spindles_with_threshold(
@@ -109,57 +108,44 @@ list(
       pattern = cross(freqs, channels)
     ),
     tar_target(
-      final_dataset,
+      channel_avg_dataset,
       average_channel_data(dataset),
       pattern = map(dataset)
     ),
     tar_target(
-      long_dataset,
+      md,
       {
-        pivot_vars <- setdiff(
-          names(final_dataset),
-          c("bach_id", "channel", "freq")
+        setnames(channel_avg_dataset, "bach_id", "ID")
+        merge(channel_avg_dataset, redcap_data, by = "ID", all.x = TRUE)
+      },
+      pattern = map(channel_avg_dataset)
+    ),
+    tar_target(
+      model_summary,
+      {
+        formula_str <- paste0(
+          outcomes,
+          " ~ ",
+          predictors,
+          " + age + sex + education_centered + apoe_e4_status + psg_ahi_total_nrem + psg_tst + psg_waso"
         )
-        dcast(final_dataset, bach_id ~ channel + freq, value.var = pivot_vars)
-      }
+        model <- lm(as.formula(formula_str), data = md)
+        summary <- summary(model)
+        result <- as.data.table(summary$coefficients, keep.rownames = TRUE)[
+          rn == predictors,
+        ]
+        setnames(result, "rn", "predictor")
+        md[, list(ID, freq, channel)]
+        result[,
+          c("outcome", "freq", "channel", "n") := list(
+            outcomes,
+            unique(md$freq),
+            unique(md$channel),
+            sum(!is.na(md[[predictors]]))
+          )
+        ]
+      },
+      pattern = cross(cross(outcomes, predictors), md)
     )
-  ),
-  tar_target(
-    filtered_results,
-    filter_spindles_so(threshold_results),
-    pattern = map(threshold_results)
-  ),
-  tar_target(
-    freqs,
-    c(11, 15)
-  ),
-  tar_target(
-    channels,
-    c("C", "F")
-  ),
-  tar_target(
-    cleaned_results,
-    clean_angle(filtered_results),
-    pattern = map(filtered_results)
-  ),
-  tar_target(
-    dataset,
-    cleaned_results[F == freqs & grepl(channels, CH), ],
-    pattern = cross(freqs, channels)
-  ),
-  tar_target(
-    final_dataset,
-    average_channel_data(dataset),
-    pattern = map(dataset)
-  ),
-  tar_target(
-    long_dataset,
-    {
-      pivot_vars <- setdiff(
-        names(final_dataset),
-        c("bach_id", "channel", "freq")
-      )
-      dcast(final_dataset, bach_id ~ channel + freq, value.var = pivot_vars)
-    }
   )
 )
